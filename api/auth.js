50b0cb363d25408c306b18865a6992e6c4df9040
gummy1803-ai <gummy1803@gmail.com>
2026-06-03 12:16:44 +0800

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
-  const APP_ID = process.env.FEISHU_APP_ID;
-  const REDIRECT_URI = encodeURIComponent(process.env.REDIRECT_URI);
-  const scope = encodeURIComponent("wiki:wiki:readonly sheets:spreadsheet:readonly");
-  const url = `https://open.feishu.cn/open-apis/authen/v1/authorize?app_id=${APP_ID}&redirect_uri=${REDIRECT_URI}&scope=${scope}`;
+  const FEISHU_APP_ID = process.env.FEISHU_APP_ID;
+  const REDIRECT_URI  = process.env.REDIRECT_URI;
+  const scope = "wiki:wiki:readonly sheets:spreadsheet";
+  const url = `https://open.feishu.cn/open-apis/authen/v1/authorize?app_id=${FEISHU_APP_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(scope)}`;
   res.redirect(url);
 }
